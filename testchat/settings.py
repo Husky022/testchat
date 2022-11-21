9514f95a91b697b827002bae2b4ff7b7220d4dcd
husky022 <balashovia@bk.ru>
2022-11-22 00:08:03 +0300

    added secrets 2.0

--- testchat/settings.py
+++ testchat/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import secrets
+import proj_secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets.key
+SECRET_KEY = proj_secrets.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
